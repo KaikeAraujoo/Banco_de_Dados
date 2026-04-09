70216ed5d816a29f1c61755a02ae36312659e7f9
Kaike Araújo <kaikeuniespfds@gmail.com>
2026-04-09 11:51:08 -0300

    Delete atividade_de_BD.sql

--- atividade_de_BD.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    p.nome,
-    COALESCE(SUM(iv.quantidade), 0) AS unidades_vendidas
-FROM produtos p
-LEFT JOIN itens_venda iv 
-    ON iv.produto_id = p.id
-GROUP BY p.id, p.nome
-ORDER BY unidades_vendidas DESC;
